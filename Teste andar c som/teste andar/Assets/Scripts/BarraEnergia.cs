8be7181ee1e3a4994fdc4ca789a656986eea3948
ClaraAlonso <claraalonsoleao@gmail.com>
2017-05-01 20:15:13 -0300

    Primeira versão
    Primeira versão pronta

---
+++ Teste andar c som/teste andar/Assets/Scripts/BarraEnergia.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BarraEnergia : MonoBehaviour {
+    public Image barraEnergia;
+    public static float energiaMax = 10, energiaAtual;
+    void Start()
+    {
+        energiaMax = energiaAtual;
+    }
+    void Update () {
+        if (energiaAtual >= energiaMax)
+        {
+            energiaAtual = energiaMax;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            barraEnergia.rectTransform.sizeDelta = new Vector2(energiaAtual / energiaMax * 590, 34);
+
+        }
+
+    }
+}
---
+++ Teste andar c som/teste andar/Assets/Scripts/Ventilador.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Ventilador : MonoBehaviour {
+    public Text mensagem;
+    public Rigidbody ventilador;
+    private bool ligado = false;
+    
+    void OnTriggerEnter(Collider other)
+    {
+     mensagem.text = "Aperte V para ligar/desligar o ventilador.";
+         if(Input.GetKeyDown(KeyCode.V))
+             {
+            if (ligado == false)
+            {
+                ventilador.AddForce(10, 0 , 0);
+                ligado = true;
+                mensagem.text = "";
+            }
+            else {
+                ventilador.AddForce(0, 0, 0);
+                ligado = false;
+                mensagem.text = "";
+            }
+        }
+        mensagem.text = "";
+    }
+    
+}
