51dcc2be27c98bffa3609e62742d1c4083b7c852
mateushmdz <mateushmdz@gmail.com>
2017-05-16 12:25:56 -0300

    1o teste de ligar o ventilador
    O ventilador está ligando qnd o personagem passa na caixa, falta
    verificar e receber o space pra ligar o ventilador e trocar isto para o
    projetor.

--- Teste andar c som/teste andar/Assets/Scripts/Ventilador.cs
+++ Teste andar c som/teste andar/Assets/Scripts/Ventilador.cs
@@ -3,27 +3,11 @@ using UnityEngine.UI;
 
 public class Ventilador : MonoBehaviour {
     public Text mensagem;
-    public Rigidbody ventilador;
-    private bool ligado = false;
-    
+    public Rigidbody ventilador;    
     void OnTriggerEnter(Collider other)
     {
      mensagem.text = "Aperte V para ligar/desligar o ventilador.";
-         if(Input.GetKeyDown(KeyCode.V))
-             {
-            if (ligado == false)
-            {
-                ventilador.AddForce(10, 0 , 0);
-                ligado = true;
-                mensagem.text = "";
-            }
-            else {
-                ventilador.AddForce(0, 0, 0);
-                ligado = false;
-                mensagem.text = "";
-            }
-        }
-        mensagem.text = "";
+                ventilador.AddRelativeTorque(0,20,0);          
     }
     
 }
