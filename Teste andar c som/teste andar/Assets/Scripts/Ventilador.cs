b0b3bfb13fc2baf59c94e316f32c9f512899df7c
mateushmdz <mateushmdz@gmail.com>
2017-05-20 14:19:52 -0300

    Ventilador funcionando
    Ventilador na sala 1001 funcionando quando passa no interruptor

--- Teste andar c som/teste andar/Assets/Scripts/Ventilador.cs
+++ Teste andar c som/teste andar/Assets/Scripts/Ventilador.cs
@@ -2,14 +2,33 @@
 using UnityEngine.UI;
 
 public class Ventilador : MonoBehaviour {
-    public Text mensagem;
-    public Rigidbody ventilador;    
+    //public Text mensagem;
+    public Rigidbody ventilador;
+    public Rigidbody ventilador2;
+    public bool ligado = false;
+
     void OnTriggerEnter(Collider other)
     {
-     mensagem.text = "16 ";
-                ventilador.AddRelativeTorque(0,150,0);
-        //static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
-
+        //mensagem.text = "16 ";
+        if (ligado == false)
+        {
+            ventilador.AddRelativeTorque(0, 300, 0);
+            ventilador2.AddRelativeTorque(0, 300, 0);
+            //static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+            ligado = true;
+        }
+        else
+        {
+            ventilador.AddRelativeTorque(0, -150, 0);
+            ventilador2.AddRelativeTorque(0, -150, 0);
+            ventilador.AddRelativeTorque(0, 0, 0);
+            ventilador2.AddRelativeTorque(0, 0, 0);
+            ventilador.AddRelativeTorque(0, -50, 0);
+            ventilador2.AddRelativeTorque(0, -50, 0);
+            ventilador.AddRelativeTorque(0, 0, 0);
+            ventilador2.AddRelativeTorque(0, 0, 0);
+            ligado = false;
+        }
     }
 
 }
