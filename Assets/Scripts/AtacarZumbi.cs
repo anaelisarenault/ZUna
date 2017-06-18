18a50c0baa7e7107f13c762f60f6bf874c6672bf
ClaraAlonso <claraalonsoleao@gmail.com>
2017-06-18 18:55:19 -0300

    braços
    animacao de atacar mas ainda nao sei como coloca

---
+++ Assets/Scripts/AtacarZumbi.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AtacarZumbi : MonoBehaviour {
+    [Range(0.1f, 10.0f)]
+    private float distancia = 1.5f;
+    private GameObject Jogador;
+    public int zumbividaAtual = 12;   
+    void Start () {
+        
+        Jogador = GameObject.FindWithTag("Player");
+    }
+   
+void Update () {
+        
+            if (Vector3.Distance(transform.position, Jogador.transform.position) < distancia)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    zumbividaAtual = zumbividaAtual - 3;
+                }
+            }            
+        
+        if (zumbividaAtual == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
